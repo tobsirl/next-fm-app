6313e2d75a0695df1e3f1dbb7a5d84580d5b5ffd
Paul Tobin <tobster111@gmail.com>
2023-02-27 18:27:27 +0000

    Next-FM-App GreetingsSkeletion suspense

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,4 +1,5 @@
 import Greetings from '@/components/Greetings'
+import GreetingsSkeleton from '@/components/GreetingsSkeleton'
 import { delay } from '@/lib/async'
 import { getUserFromCookie } from '@/lib/auth'
 import { db } from '@/lib/db'
@@ -11,7 +12,10 @@ export default async function Page() {
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
+          <Suspense fallback={<GreetingsSkeleton />}>
+
           <Greetings />
+          </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
