88d0da146f3cc7550d3c8d7a5d30ce95903e5738
Paul Tobin <tobster111@gmail.com>
2023-02-28 18:23:02 +0000

    Next-FM-App map over projects

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -23,6 +23,8 @@ const getData = async () => {
 };
 
 export default async function Page() {
+  const { projects } = await getData();
+
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
@@ -32,7 +34,14 @@ export default async function Page() {
           </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-          {/** projects map here */}
+          { projects.map((project) => (
+      <div className="w-1/3 p-3" key={project.id}>
+        <Link href={`/project/${project.id}`}>
+          <ProjectCard project={project} />
+        </Link>
+      </div>
+    ))
+  }
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
