aa48e555e075456859e327ff943a232d68bbba72
Paul Tobin <tobster111@gmail.com>
2023-02-28 18:23:24 +0000

    Next-FM-App map over projects

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -8,22 +8,22 @@ import Link from 'next/link'
 import { Suspense } from 'react'
 
 const getData = async () => {
-  await delay(2000);
-  const user = await getUserFromCookie(cookies());
+  await delay(2000)
+  const user = await getUserFromCookie(cookies())
   const projects = await db.project.findMany({
     where: {
-      ownerId: user.id,
+      ownerId: user?.id,
     },
     include: {
       tasks: true,
     },
-  });
+  })
 
-  return { projects };
-};
+  return { projects }
+}
 
 export default async function Page() {
-  const { projects } = await getData();
+  const { projects } = await getData()
 
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
@@ -34,14 +34,13 @@ export default async function Page() {
           </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-          { projects.map((project) => (
-      <div className="w-1/3 p-3" key={project.id}>
-        <Link href={`/project/${project.id}`}>
-          <ProjectCard project={project} />
-        </Link>
-      </div>
-    ))
-  }
+          {projects.map((project) => (
+            <div className="w-1/3 p-3" key={project.id}>
+              <Link href={`/project/${project.id}`}>
+                <ProjectCard project={project} />
+              </Link>
+            </div>
+          ))}
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
