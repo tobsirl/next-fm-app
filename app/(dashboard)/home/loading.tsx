85ae149c4d75d77799aaf63a709e958b69e18741
Paul Tobin <tobster111@gmail.com>
2023-03-01 17:13:09 +0000

    Next-FM-App loading

--- app/(dashboard)/home/loading.tsx
+++ app/(dashboard)/home/loading.tsx
@@ -1,4 +1,4 @@
-import Card from "@/components/Card";
+import Card from '@/components/Card'
 
 export default function HomePageLoader() {
   return (
@@ -7,5 +7,5 @@ export default function HomePageLoader() {
         <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
       </Card>
     </div>
-  );
+  )
 }
